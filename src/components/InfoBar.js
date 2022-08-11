dbd6e902a5f399d241c5c36d05e7f7c759a3deae
Ilia Alekseev <iliaalekseevofb@gmail.com>
2022-08-12 03:08:22 +0500

    Infobar and Navbar are responsive, but not functional yet

--- src/components/InfoBar.js
+++ src/components/InfoBar.js
@@ -1,16 +1,41 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { ImLocation2 } from 'react-icons/im';
+import { BsFillTelephoneFill } from 'react-icons/bs';
+import { FiMenu } from 'react-icons/fi';
 
 const InfoBar = () => {
   return (
-    <div className='w-full h-16 sm:h-20 xl:h-24 p-2 lg:p-3 flex justify-center items-center border-b border-neutral-200'>
+    <div className='w-full h-20 sm:h-24 xl:h-28 py-1 px-3 lg:py-3 lg:px-5 flex justify-center items-center border-b border-neutral-200'>
       <div className='w-full h-full max-w-screen-2xl flex justify-between items-center'>
         <div className='flex justify-center items-start flex-col'>
-          <h1 className='text-3xl leading-normal font-sans font-bold'>Домик "У Барта"</h1>
-          <p className='text-xl leading-normal font-serif'>Живите в 7 минутах от моря</p>
+          <h1 className='text-xl sm:text-3xl text-green-900 leading-normal font-sans font-bold'>Домик "У Барта"</h1>
+          <p className='text-md sm:text-xl leading-normal font-serif'>Живите в 7 минутах от моря</p>
         </div>
-        <div>
-          hello
+        <div className='flex justify-end items-center sm:grid sm:grid-cols-3 lg:grid-cols-2 xl:grid-cols-3 gap-2 lg:gap-0'>
+          <div className='flex justify-end items-center'>
+            <ImLocation2 size={36} className='hidden sm:block text-green-600 mr-3' />
+            <div className='hidden lg:block'>
+              <h3 className='text-xl font-sans'>Где мы находимся?</h3>
+              <p className='font-serif text-neutral-500'>г. Севастополь, п. Мирный,<br />ул. Каскадная, дом 17</p>
+            </div>
+          </div>
+          <div className='flex justify-end items-center'>
+            <BsFillTelephoneFill size={32} className='hidden sm:block text-green-600 mr-3' />
+            <div className='hidden lg:block'>
+              <h3 className='text-xl font-sans'>Как связаться?</h3>
+              <p className='font-serif text-neutral-500'>+ 7 (987) 65-43-210</p>
+              <p className='font-serif text-neutral-500'>+ 7 (123) 45-67-890</p>
+            </div>
+          </div>
+          <div className='hidden xl:flex justify-end items-center'>
+            <button className='px-4 py-3 rounded-2xl bg-green-600 hover:bg-green-700 duration-200 font-serif text-xl font-bold text-white'>
+              Заказать звонок
+            </button>
+          </div>
+          <div className='flex lg:hidden justify-end items-center'>
+            <FiMenu size={40} />
+          </div>
         </div>
       </div>
     </div>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,11 +4,11 @@ const Navbar = () => {
   const navbarContent = ['О нас', 'Проживание', 'Галерея', 'Спецпредложения', 'Отзывы', 'Расположение', 'Контакты']
 
   return (
-    <div className='w-full h-12 xl:h-16 p-1 lg:p-3 flex justify-center items-center shadow-lg'>
+    <div className='w-full h-16 py-1 px-3 lg:py-3 lg:px-5 hidden lg:flex justify-center items-center shadow-lg'>
       <div className='w-full h-full p-1 max-w-screen-2xl flex justify-between items-center'>
         {navbarContent.map((item, key) => (
           <div key={key} className='flex justify-center items-center'>
-            <p className='text-lg font-serif font-semibold hover:text-green-500 duration-100 cursor-pointer'>{item}</p>
+            <p className='text-lg font-serif font-semibold hover:text-green-600 duration-100 cursor-pointer'>{item}</p>
           </div>
         ))}
       </div>
