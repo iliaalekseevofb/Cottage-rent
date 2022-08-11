98a4644c406492fbb4ffd2dc5cdc26e610c5697c
Ilia Alekseev <iliaalekseevofb@gmail.com>
2022-08-12 04:32:46 +0500

    Error page is added

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,3 +3,4 @@ export {default as Room1} from './Room1';
 export {default as Room2} from './Room2';
 export {default as Room3} from './Room3';
 export {default as Room4} from './Room4';
+export {default as ErrorPage} from './ErrorPage';
