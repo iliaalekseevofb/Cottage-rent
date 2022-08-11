1c6a89c107614a52d520a79a5ad6bc432a6f303d
Ilia Alekseev <iliaalekseevofb@gmail.com>
2022-08-12 04:16:55 +0500

    Infobar and Navbar are almost fully functional

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,14 +1,22 @@
 import React from 'react';
 
-const Navbar = () => {
-  const navbarContent = ['О нас', 'Проживание', 'Галерея', 'Спецпредложения', 'Отзывы', 'Расположение', 'Контакты']
+const navbarContent = [
+  ['О нас', '#about'], 
+  ['Проживание', '#accomodation'], 
+  ['Галерея', '#gallery'], 
+  ['Спецпредложения', '#discounts'], 
+  ['Отзывы', '#testimonials'], 
+  ['Расположение', '#location'], 
+  ['Контакты', '#contacts']
+]
 
+const Navbar = () => {
   return (
-    <div className='w-full h-16 py-1 px-3 lg:py-3 lg:px-5 hidden lg:flex justify-center items-center shadow-lg'>
+    <div className='w-full h-16 py-1 lg:py-3 px-5 hidden lg:flex justify-center items-center shadow-lg'>
       <div className='w-full h-full p-1 max-w-screen-2xl flex justify-between items-center'>
         {navbarContent.map((item, key) => (
           <div key={key} className='flex justify-center items-center'>
-            <p className='text-lg font-serif font-semibold hover:text-green-600 duration-100 cursor-pointer'>{item}</p>
+            <a href={item[1]} className='text-lg font-serif font-semibold hover:text-green-600 duration-100 cursor-pointer'>{item[0]}</a>
           </div>
         ))}
       </div>
