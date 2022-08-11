acd010390e2843a6e2d1925019376c47476b8944
Ilia Alekseev <iliaalekseevofb@gmail.com>
2022-08-11 15:11:01 +0500

    Necessary dependencies are installed

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
 import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import { Home, Room1, Room2, Room3, Room4 } from './pages';
+import { Navbar, Footer } from './components';
 
 const App = () => {
   return (
-    <div>App</div>
+    <div className='app'>
+      <h1 className='text-3xl underline'>Hellloooooo</h1>
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/room1' element={<Room1 />} />
+        <Route path='/room2' element={<Room2 />} />
+        <Route path='/room3' element={<Room3 />} />
+        <Route path='/room4' element={<Room4 />} />
+      </Routes>
+      <Footer />
+    </div>
   )
 }
 
---
+++ src/pages/index.js
@@ -0,0 +1,5 @@
+export {default as Home} from './Home';
+export {default as Room1} from './Room1';
+export {default as Room2} from './Room2';
+export {default as Room3} from './Room3';
+export {default as Room4} from './Room4';
