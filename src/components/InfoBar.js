a00d2c6a1a458509229e40ce568f365881429b84
Ilia Alekseev <iliaalekseevofb@gmail.com>
2022-08-12 21:41:05 +0500

    Infobar and Navbar are done (without smooth animation

--- src/components/InfoBar.js
+++ src/components/InfoBar.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { Link } from 'react-router-dom';
 import { ImLocation2 } from 'react-icons/im';
 import { BsFillTelephoneFill } from 'react-icons/bs';
@@ -18,10 +18,20 @@ const phoneNumbers = ['+ 7 (987) 65-43-210', '+ 7 (123) 45-67-890'];
 
 const InfoBar = () => {
   const [toggleMenu, setToggleMenu] = useState(false);
+
+  const menuRef = useRef();
+  useEffect(() => {
+    let menuHandler = (e) => {
+      if (!menuRef.current.contains(e.target))
+      setToggleMenu(false);
+    };
+    document.addEventListener('mousedown', menuHandler)
+  });
+
   const DropdownMenu = () => {
     return (
       <div className={`absolute ${toggleMenu ? 'block' : 'hidden'} lg:hidden top-20 sm:top-24 right-0 left-0 bg-white`}>
-        <a onClick={() => {setToggleMenu(!toggleMenu)}} href='#location' className='h-16 px-5 flex justify-start items-center border-b border-b-neutral-200'>
+        <a href='#contacts' onClick={() => {setToggleMenu(false)}} className='h-16 px-5 flex justify-start items-center border-b border-b-neutral-200'>
           <BsFillTelephoneFill size={24} className='text-green-700 mr-2'/>
           <div className='font-serif'>
             <p>{phoneNumbers[0]}</p>
@@ -30,7 +40,7 @@ const InfoBar = () => {
         </a>
         <div className='flex flex-col justify-center items-start text-xl font-serif bg-neutral-100'>
           {navbarContent.map((item, key) => (
-            <a onClick={() => {setToggleMenu(!toggleMenu)}} key={key} href={item[1]} className='w-full h-16 px-5 flex justify-start items-center font-medium border-b border-neutral-200'>
+            <a key={key} onClick={() => {setToggleMenu(false)}} href={item[1]} className='w-full h-16 px-5 flex justify-start items-center font-medium border-b border-neutral-200'>
               {item[0]}
             </a>
           ))}
@@ -43,7 +53,7 @@ const InfoBar = () => {
     <div className='w-full h-20 sm:h-24 xl:h-28 py-1 lg:py-3 px-5 flex justify-center items-center border-b border-neutral-200'>
       <div className='w-full h-full max-w-screen-2xl flex justify-between items-center'>
         {/* Logo */}
-        <Link onClick={() => {setToggleMenu(!toggleMenu)}} to='/'>
+        <Link to='/'>
           <div className='flex justify-center items-start flex-col'>
             <h1 className='text-xl sm:text-3xl text-green-900 leading-normal font-sans font-bold'>Домик "У Барта"</h1>
             <p className='text-md sm:text-xl leading-normal font-serif'>Живите в 7 минутах от моря</p>
@@ -77,12 +87,14 @@ const InfoBar = () => {
               Заказать звонок
             </button>
           </div>
-          {/* Menu icon */}
-          <button onClick={() => {setToggleMenu(!toggleMenu)}} className='flex lg:hidden justify-end items-center ml-5'>
-            <FiMenu size={40} />
-          </button>
-          {/* Dropdown menu */}
-          <DropdownMenu />
+          <div ref={menuRef}>
+            {/* Menu icon */}
+            <button onClick={() => {setToggleMenu(!toggleMenu)}} className='flex lg:hidden justify-end items-center ml-5'>
+              <FiMenu size={40} />
+            </button>
+            {/* Dropdown menu */}
+            <DropdownMenu />
+          </div>
         </div>
       </div>
     </div>
