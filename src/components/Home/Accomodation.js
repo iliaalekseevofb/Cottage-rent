bcba2b185c999a1c2f6069337d72984cb1b6cfc9
Ilia Alekseev <iliaalekseevofb@gmail.com>
2022-08-17 20:42:51 +0500

    Dynamic routes are added

--- src/components/Home/Accomodation.js
+++ src/components/Home/Accomodation.js
@@ -1,4 +1,5 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Accomodation = () => {
   return (
@@ -8,8 +9,11 @@ const Accomodation = () => {
           <h1 className='text-3xl lg:text-4xl font-sans pb-2'>Наши номера</h1>
           <p className='w-12 lg:w-20 border-t-4 border-green-600'></p>
         </div>
-        <div>
-
+        <div className='mt-12'>
+          <Link to='/room-1'>Room 1</Link>
+          <Link to='/room-2'>Room 2</Link>
+          <Link to='/room-3'>Room 3</Link>
+          <Link to='/room-4'>Room 4</Link>
         </div>
       </div>
     </div>
--- src/pages/RoomDetails.js
+++ src/pages/RoomDetails.js
@@ -1,15 +1,31 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
+import { amyHumphries1, amyHumphries2, andresMolina, andrewSpencer, clayBanks, coleAnkney, hansIsaacson1, hansIsaacson2, julianHochgesang, justinKauffman, loeweTechnology, samManns } from '../assets';
 
 const roomInfo = [
-  
+  ['1', [amyHumphries1, amyHumphries2, andresMolina]],
+  ['2', [andrewSpencer, clayBanks, coleAnkney]],
+  ['3', [hansIsaacson1, hansIsaacson2, julianHochgesang]],
+  ['4', [justinKauffman, loeweTechnology, samManns]]
 ]
 
 const RoomDetails = () => {
   const {id} = useParams();
 
   return (
-    <div>RoomDetails</div>
+    <div>
+      {roomInfo.map((item, itemIndex) => {
+          return (id === item[0] &&
+            <div key={itemIndex}>
+              {item[1].map((image, imageIndex) => (
+                <div key={imageIndex}>
+                  <img src={image} alt='...' />
+                </div>
+              ))}
+            </div>
+          )
+      })}
+    </div>
   )
 }
 
