a49dbc732092fcaa4af3a08dc23f0e2073718af7
Ilia Alekseev <iliaalekseevofb@gmail.com>
2022-08-19 02:53:33 +0500

    Room details page is partially done (not responsive, not functional)

--- src/pages/RoomDetails.js
+++ src/pages/RoomDetails.js
@@ -3,13 +3,23 @@ import { useParams } from 'react-router-dom';
 import { amyHumphries1, amyHumphries2, andresMolina, andrewSpencer, clayBanks, coleAnkney, hansIsaacson1, hansIsaacson2, julianHochgesang, justinKauffman, loeweTechnology, samManns } from '../assets';
 import { IoIosArrowForward, IoIosArrowBack } from 'react-icons/io';
 import { BsFillPeopleFill } from 'react-icons/bs';
-import { FaSmileBeam, FaBed, FaCouch, FaShower } from 'react-icons/fa';
+import { FaSmileBeam, FaBed, FaCouch, FaShower, FaInfoCircle } from 'react-icons/fa';
 
 const roomInfo = [
   ['1', [amyHumphries1, amyHumphries2, andresMolina], 'Номер "Делюкс"', 3290],
   ['2', [andrewSpencer, clayBanks, coleAnkney], 'Номер "Стандарт"', 2590],
   ['3', [hansIsaacson1, hansIsaacson2, julianHochgesang], 'Номер "Эконом"', 1990],
   ['4', [justinKauffman, loeweTechnology, samManns], 'Номер "Люкс"', 4790]
+]
+
+const characteristicsContent = [
+  [<BsFillPeopleFill size={28} className='text-orange-500' />, 'Гостей:', '3'],
+  [<FaSmileBeam size={28} className='text-orange-500' />, 'Удобства:', 'Wi-Fi, телевизор, холодильник, фен, ванная, балкон'],
+  [<FaBed size={28} className='text-orange-500' />, 'Тип кровати:', 'Большая двуспальная кровать'],
+  [<FaCouch size={28} className='text-orange-500' />, 'Гостинные:', '1'],
+  [<FaShower size={28} className='text-orange-500' />, 'Ванные:', '1'],
+  [<FaInfoCircle size={28} className='text-orange-500' />, 'Площадь:', '25 м2'],
+  [<FaInfoCircle size={28} className='text-orange-500' />, 'Этаж:', '2-ой']
 ]
 
 const RoomDetails = () => {
@@ -37,7 +47,7 @@ const RoomDetails = () => {
     <div>
       {roomInfo.map((item, itemIndex) => {
           return (id === item[0] &&
-            <div key={itemIndex} className='w-full px-5 flex justify-center items-center pt-8 lg:pt-12 bg-neutral-100'>
+            <div key={itemIndex} className='w-full px-5 flex justify-center items-center py-8 lg:py-12 bg-neutral-100'>
               <div className='w-full max-w-screen-2xl'>
                 <div className='w-full mb-8'>
                   <h1 className='text-3xl font-serif font-semibold border-l-[6px] border-green-600 pl-3'>{roomInfo[itemIndex][2]}</h1>
@@ -68,16 +78,38 @@ const RoomDetails = () => {
                     <p className='mt-6 text-lg font-serif pb-6 border-b border-neutral-300'>Эти Аппартаменты расположены на третьем этаже гостиничного здания и рассчитаны на два человека (+ 2 доп. места ). В аппартаментах: спальня с двухспальной кроватью и гостинная комната с двухспальным раскладным диваном, ТВ, кухня и постельное белье. Один саузел с душевой комнатой. Рядом с гостиницей расположена зона барбекю общего пользования со столиками и зоной отдыха.</p>
                     <div className='w-full mt-8 flex justify-between items-center'>
                       <button className='mr-3 w-1/2 py-4 text-white text-xl rounded-md bg-green-600 hover:brightness-75 duration-200'>Оставить заявку</button>
-                      <button className='ml-3 w-1/2 py-4 text-green-600 text-xl rounded-md bg-white border-2 border-green-600 hover:brightness-75 duration-200'>Подробнее</button>
+                      <a href='#description' className='ml-3 w-1/2 py-4 flex justify-center items-center text-green-600 text-xl rounded-md bg-white border-2 border-green-600 hover:brightness-75 duration-200'>Подробнее</a>
                     </div>
                   </div>
                 </div>
-                <div className='w-full h-[500px] bg-white rounded-lg shadow-lg'>
-                  <div>
-                    {/* Precise desription */}
+                <div id='description' className='w-full h-[500px] p-8 bg-white rounded-lg shadow-lg flex justify-between items-start'>
+                  <div className='w-1/2 h-full border-r border-neutral-300 pr-8'>
+                    <h1 className='text-3xl font-semibold mb-6 pl-4 border-l-4 border-green-600'>Описание</h1>
+                    <p className='text-lg font-serif'>Эти Апартаменты расположены на третьем этаже гостиничного здания и рассчитаны на два человека (+ 2 доп. места ).<br/><br />В аппартаментах: спальная комната с двухспальной кроватью, комодом и шкафом. Кухонная зона в гостинной комнате оборудована электрочайником, электроплитой, холодильником, микроволновой печью, посудой и всем самым необходимым для приготовления питания, а так же обеденным столом на 4 человека. В гостиной зоне расположен двуспальный раскладывающийся диван, ТВ, комод, шкаф для одежды, постельное белье. Один санузел с душевой комнатой.<br /><br />Рядом с гостиницей расположена зона барбекю общего пользования со столиками и зоной отдыха.</p>
                   </div>
-                  <div>
-                    {/* Characteristics */}
+                  <div className='w-1/2 h-full pl-8'>
+                    <h1 className='text-3xl font-semibold mb-6 pl-4 border-l-4 border-green-600'>Характеристики</h1>
+                    <div className='flex justify-between items-center'>
+                        <div>
+                          <ul>
+                            {characteristicsContent.map((charItem, charIndex) => (
+                              <li key={charIndex} className='flex justify-start items-center pb-4'>
+                                {charItem[0]}
+                                <p className='ml-4 text-lg text-neutral-500 font-serif'>{charItem[1]}</p>
+                              </li>
+                            ))}
+                          </ul>
+                        </div>
+                        <div>
+                          <ul>
+                            {characteristicsContent.map((charItem, charIndex) => (
+                              <li key={charIndex} className='flex justify-start items-center pb-4'>
+                                <p className='ml-4 text-lg font-medium font-serif'>{charItem[2]}</p>
+                              </li>
+                            ))}
+                          </ul>
+                        </div>
+                    </div>
                   </div>
                 </div>
               </div>
