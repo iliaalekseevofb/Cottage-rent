a3e8c9ec27ecafe80e2eda3e13e41a45c78f91c3
Ilia Alekseev <iliaalekseevofb@gmail.com>
2022-08-28 01:49:05 +0300

    Second part of the footer is partially done (not responsive, not functional)

--- src/components/Common/Footer.js
+++ src/components/Common/Footer.js
@@ -1,5 +1,13 @@
 import React from 'react';
 import { IoIosArrowForward } from 'react-icons/io';
+import { ImLocation2 } from 'react-icons/im';
+import { AiFillInstagram, AiFillTwitterSquare } from 'react-icons/ai';
+
+const footerContent = [
+  ['О базе отдыха', 'Проживание', 'Мероприятия'],
+  ['Рыбалка', 'Дни рождения', 'Развлечения', 'Контакты'],
+  ['Охота', 'Свадьбы', 'Цены']
+]
 
 const Footer = () => {
   return (
@@ -14,7 +22,37 @@ const Footer = () => {
           <button className='ml-4 py-3 w-44 border-2 border-white text-white rounded-md hover:bg-myBackground duration-200'>Задать вопрос</button>
         </div>
       </div>
-      <div className='w-full h-[300px] bg-neutral-700'></div>
+      <div className='w-full py-8 bg-neutral-700 flex justify-center items-center'>
+        <div className='w-full py-4 max-w-screen-xl'>
+          <div className='flex justify-between items-start'>
+            <div className='w-64 flex justify-between items-start'>
+              <ImLocation2 className='text-orange-500' size={24}/>
+              <p className='w-5/6 text-white font-serif'>Загородная база отдыха «Домик у Барта» расположена в одноименной живописной деревушке Краснокамского района Подмосковья на 127 километре Каширского шоссе.</p>
+            </div>
+            <div className='w-3/4 grid grid-cols-4 grid-rows-1 gap-6'>
+              {footerContent.map((item, index) => (
+                <div key={index} className='flex flex-col justify-start items-start'>
+                  {item.map((innerItem, innerIndex) => (
+                    <a className='text-white text-md font-serif pb-2 hover:text-orange-500 duration-200' key={innerIndex} href='/'>{innerItem}</a>
+                  ))}
+                </div>
+              ))}
+              <div>
+                <h5 className='text-orange-500 pb-2'>Мы в соцсетях:</h5>
+                <div className='w-16 flex justify-between items-center'>
+                  <a href='/'>
+                    <AiFillInstagram className='text-white hover:text-orange-500 duration-200' size={32} />
+                  </a>
+                  <a href='/'>
+                    <AiFillTwitterSquare className='text-white hover:text-orange-500 duration-200' size={32} />
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div></div>
+        </div>
+      </div>
     </div>
   )
 }
